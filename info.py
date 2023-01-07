eb07989c636b5bbdf351a0b94a04d117992f5bea
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-01-07 08:22:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Royaldwip:Royaldwip@cluster0.z80w7b7.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Akpicturebot:Akpicturebot@cluster0.88tpl4c.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Akpicture")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
