fdf65136b91551e7c2ab14c86dd80d2913e53d91
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-01-17 20:05:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ API_HASH = environ.get('API_HASH', 'cd6eef261797436a6fe2b7cbf1630320')
 BOT_TOKEN = environ.get('BOT_TOKEN', "5797499618:AAFD4xCRgMCKxxoRrkKwIw1h74cHkSngOO4")
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+CACHE_TIME = int(environ.get('CACHE_TIME', 3600))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/33b0ed76884a7caf41dc1.jpg')).split()
